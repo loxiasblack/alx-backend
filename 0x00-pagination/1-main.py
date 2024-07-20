d04bbe6f712a973b102832ed4d3d43f6fa4cf7ae
loxiasblack <iliaschokairi@gmail.com>
2024-07-20 09:42:56 +0100

    let the function index_range be a method of class

--- 0x00-pagination/1-main.py
+++ 0x00-pagination/1-main.py
@@ -27,4 +27,3 @@ except AssertionError:
 print(server.get_page(1, 3))
 print(server.get_page(3, 2))
 print(server.get_page(3000, 100))
-print(server.get_page(19418, 2))
--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,13 +5,6 @@ import math
 from typing import List
 
 
-def index_range(page: int, page_size: int) -> tuple:
-    """ index range function"""
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_index)
-
-
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -31,11 +24,17 @@ class Server:
 
         return self.__dataset
 
+    def index_range(self, page: int, page_size: int) -> tuple:
+        """ index range function"""
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return (start_index, end_index)
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """implement get_page function"""
-        assert type(page) == int and page > 0
-        assert type(page_size) == int and page_size > 0
-        res = index_range(page, page_size)
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        res = self.index_range(page, page_size)
         dataset = self.dataset()
         if res[0] >= len(dataset):
             return []
