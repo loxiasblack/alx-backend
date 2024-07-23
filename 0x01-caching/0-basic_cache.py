904551db5d29574aa9a03d73725b948493ff6db5
loxiasblack <iliaschokairi@gmail.com>
2024-07-23 21:56:36 +0100

    0-baskic

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""0. Basic dictionary"""
+""" 0. Basic dictionary"""
 
 from base_caching import BaseCaching
 
@@ -8,11 +8,13 @@ class BasicCache(BaseCaching):
     """class BasicCache"""
 
     def put(self, key, item):
+        """ put function """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """ get function """
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
