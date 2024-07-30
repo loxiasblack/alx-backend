605ad5f733bd5a431a06ce3ff93dad6ee537530b
loxiasblack <iliaschokairi@gmail.com>
2024-07-30 22:19:39 +0100

    push

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
-""" new introduction to config class """
+"""A simple flask app
+"""
+
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
-class Config:
-    """ the class Config """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCAL = 'en'
+class Config(object):
+    """_summary_
+
+    Returns:
+            _type_: _description_
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# configure the flask app
 app = Flask(__name__)
-
 app.config.from_object(Config)
-
 babel = Babel(app)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index():
-    """ render 1-index"""
+    """_summary_
+    """
     return render_template('1-index.html')
 
 
-if __name__ == "__main__":
-    """ run app """
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
