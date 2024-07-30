b059b159d71747dc1bdbd29a0d306d63c48b2fb1
loxiasblack <iliaschokairi@gmail.com>
2024-07-30 11:10:03 +0100

    push

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,10 +4,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """ render the first template """
     return render_template('0-index.html')
 
+
 if __name__ == "__main__":
+    """run the app"""
     app.run(host='0.0.0.0', port=5000)
