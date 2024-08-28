2ac3afcaf263143f4003921f353705e92ec646fe
loxiasblack <iliaschokairi@gmail.com>
2024-08-28 10:19:50 +0100

    push the first part that consist to work with redis until publish/subscribe comunication

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,9 @@
+const { createClient } = require('redis');
+
+const client = createClient()
+    .on('connect', () =>{
+        console.log('Redis client connected to the server');
+    })
+    .on('error', (err) => {
+        console.log(`Redis client not connected to the server: ${err}`);
+    })
